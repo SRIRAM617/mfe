67a03807d83fd90e029d6121c3dc1ecc28ebcfd7
SriramChembrolu <sriram.chembrolu@powerschool.com>
2024-01-28 12:20:15 +0530

    changed container

--- packages/container/components/MarketingApp.js
+++ packages/container/components/MarketingApp.js
@@ -3,7 +3,7 @@ import React,{useRef,useEffect} from "react";
 
 export default () => {
     const ref = useRef(null);
-    
+    console.log(ref);
     useEffect(() => {
         mount(ref.current);
     },[]);
